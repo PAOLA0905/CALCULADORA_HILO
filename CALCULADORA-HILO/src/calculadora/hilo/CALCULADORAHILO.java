0dc2bfbc82971afafa63ac88b50127dbfebd3f5e
Paola Montenegro <Paola Montenegro@PAOLATIC>
2019-07-05 20:46:30 -0500

    ACTIVIDAD 6 CALCULADORA

---
+++ CALCULADORA-HILO/src/calculadora/hilo/CALCULADORAHILO.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora.hilo;
+
+/**
+ *
+ * @author Paola Montenegro
+ */
+public class CALCULADORAHILO {
+
+   public class hilo {
+    public static void hilolargo(){
+    try{
+        Thread.sleep(2000);
+    }
+    catch (InterruptedException e){
+        System.out.println("hola");
+    }
+} 
+public static double div(int a, int b){
+        if (b==0){
+            throw new ArithmeticException("division por cero");
+        }
+        double c=a/b;
+        return c;
+    }
+}
+
+}
